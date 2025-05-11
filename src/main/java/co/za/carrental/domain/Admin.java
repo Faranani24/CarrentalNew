8878fdb5453597564ed999718684366c252ab1f6
Thabiso Kama <thabisokama400@gmail.com>
2025-05-11 13:59:57 +0200

    Service and Admin entities

--- src/main/java/co/za/carrental/domain/Admin.java
+++ src/main/java/co/za/carrental/domain/Admin.java
@@ -1,4 +1,57 @@
 package co.za.carrental.domain;
 
 public class Admin {
+    private  String adminId;
+    private  String email;
+    private  String password;
+
+    private Admin(Builder builder) {
+        this.adminId = builder.adminId;
+        this.email = builder.email;
+        this.password = builder.password;
+    }
+
+    public static class Builder {
+        private String adminId;
+        private String email;
+        private String password;
+
+
+        public void setAdminId(String adminId) {
+            this.adminId = adminId;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public Admin build() {
+            return new Admin(this);
+        }
+    }
+        public void addCar(){
+            System.out.println("Car added");
+        }
+        public void updateCar(){
+            System.out.println("Car updated");
+        }
+        public void deleteCar(){
+            System.out.println("Car deleted");
+        }
+        public void manageBookings(){
+            System.out.println("Bookings managed");
+        }
+        @Override
+        public String toString() {
+            return "Admin: "+email;
+    }
 }
+/*Admin.java
+Admin Entity class
+Author: Thabiso Kama
+Date: 11 May 2025
+ */
--- src/main/java/co/za/carrental/domain/Service.java
+++ src/main/java/co/za/carrental/domain/Service.java
@@ -1,4 +1,41 @@
 package co.za.carrental.domain;
 
 public class Service {
+    private String serviceId;
+    private String name;
+    private float costPerDay;
+
+    private Service (Builder builder){
+        this.serviceId = builder.serviceId;
+        this.name = builder.name;
+        this.costPerDay = builder.costPerDay;
+    }
+    public static class Builder{
+        private String serviceId;
+        private String name;
+        private float costPerDay;
+
+        public void setServiceId(String serviceId) {
+            this.serviceId = serviceId;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setCostPerDay(float costPerDay) {
+            this.costPerDay = costPerDay;
+        }
+        public Service build(){
+            return new Service(this);
+        }
+        @Override
+        public String toString(){
+            return name + "(R "+ costPerDay + " per day)";
+        }
+    }
 }
+/*Service.java
+Service Entity Class
+Author: Thabiso Kama
+Date: 11 May 2025
+ */
