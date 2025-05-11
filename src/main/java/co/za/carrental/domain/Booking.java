87fa7e91029b6382470d04077f2f958e4e2d1ac4
Lance1103 <230803865@mycput.ac.za>
2025-05-11 18:24:27 +0200

    commit

--- src/main/java/co/za/carrental/domain/Booking.java
+++ src/main/java/co/za/carrental/domain/Booking.java
@@ -1,4 +1,73 @@
+/*
+ * Booking.java
+ * Author: Lance Anthony Franks (230803865)
+ * Date: 11 May 2025
+ */
+
 package co.za.carrental.domain;
 
+import java.util.Date;
+
 public class Booking {
+
+    private String bookingId;
+    private Date startDate;
+    private Date endDate;
+    private Float totalCost;
+    private Enum status;
+
+    private Booking(Builder builder) {
+        this.bookingId = builder.bookingId;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+        this.totalCost = builder.totalCost;
+        this.status = builder.status;
+    }
+    public static class Builder {
+        private String bookingId;
+        private Date startDate;
+        private Date endDate;
+        private Float totalCost;
+        private Enum status;
+
+        public Builder setBookingId(String bookingId) {
+            this.bookingId = bookingId;
+            return this;
+        }
+
+        public Builder setStartDate(Date startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public Builder setEndDate(Date endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public Builder setTotalCost(Float totalCost) {
+            this.totalCost = totalCost;
+            return this;
+        }
+
+        public Builder setStatus(Enum status) {
+            this.status = status;
+            return this;
+        }
+
+        public Booking build() {
+            return new Booking(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "bookingId='" + bookingId + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", totalCost=" + totalCost +
+                ", status=" + status +
+                '}';
+    }
 }
--- src/main/java/co/za/carrental/domain/Customer.java
+++ src/main/java/co/za/carrental/domain/Customer.java
@@ -1,4 +1,90 @@
+/*
+ * Customer.java
+ * Author: Lance Anthony Franks (230803865)
+ * Date: 11 May 2025
+ */
+
 package co.za.carrental.domain;
 
+import java.util.List;
 public class Customer {
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private String licenseNumber;
+    private List<String> paymentMethods;
+
+    private Customer(Builder builder) {
+        this.customerId = builder.customerId;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.phone = builder.phone;
+        this.licenseNumber = builder.licenseNumber;
+        this.paymentMethods = builder.paymentMethods;
+    }
+    public static class Builder {
+        private String customerId;
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String password;
+        private String phone;
+        private String licenseNumber;
+        private List<String> paymentMethods;
+
+    public Builder setCustomerId(String customerId) {
+        this.customerId = customerId;
+        return this;
+    }
+    public Builder setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+    public Builder setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+    public Builder setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+    public Builder setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+    public Builder setPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+    public Builder setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+        return this;
+    }
+    public Builder setPaymentMethods(List<String> paymentMethods) {
+        this.paymentMethods = paymentMethods;
+        return this;
+    }
+    public Customer build() {
+        return new Customer(this);
+    }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", licenseNumber='" + licenseNumber + '\'' +
+                ", paymentMethods=" + paymentMethods +
+                '}';
+    }
 }
