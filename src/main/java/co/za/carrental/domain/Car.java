6438c5fbd71f0ca5f700d0300727e67e115987d3
farananikhangale <230136982@mycput.ac.za>
2025-05-11 12:44:49 +0200

    Cllases now linked to the relationship

--- src/main/java/co/za/carrental/domain/Car.java
+++ src/main/java/co/za/carrental/domain/Car.java
@@ -1,121 +1,102 @@
 package co.za.carrental.domain;
 
 public class Car {
+    private String carId;
+    private String licensePlate;
+    private String make;
+    private String model;
+    private int year;
+    private Status status;
+    private CarType carType;  // Composition with CarType
 
+    private Car(Builder builder) {
+        this.carId = builder.carId;
+        this.licensePlate = builder.licensePlate;
+        this.make = builder.make;
+        this.model = builder.model;
+        this.year = builder.year;
+        this.status = builder.status;
+        this.carType = builder.carType;
+    }
+
+    public String getCarId() { return carId; }
+    public String getLicensePlate() { return licensePlate; }
+    public String getMake() { return make; }
+    public String getModel() { return model; }
+    public int getYear() { return year; }
+    public Status getStatus() { return status; }
+    public CarType getCarType() { return carType; }
+
+    public static class Builder {
         private String carId;
         private String licensePlate;
         private String make;
         private String model;
         private int year;
         private Status status;
+        private CarType carType;
 
-        private Car(Builder builder) {
-            this.carId = builder.carId;
-            this.licensePlate = builder.licensePlate;
-            this.make = builder.make;
-            this.model = builder.model;
-            this.year = builder.year;
-            this.status = builder.status;
+        public Builder setCarId(String carId) {
+            this.carId = carId;
+            return this;
         }
 
-
-        public String getCarId() {
-            return carId;
+        public Builder setLicensePlate(String licensePlate) {
+            this.licensePlate = licensePlate;
+            return this;
         }
 
-        public String getLicensePlate() {
-            return licensePlate;
+        public Builder setMake(String make) {
+            this.make = make;
+            return this;
         }
 
-        public String getMake() {
-            return make;
+        public Builder setModel(String model) {
+            this.model = model;
+            return this;
         }
 
-        public String getModel() {
-            return model;
+        public Builder setYear(int year) {
+            this.year = year;
+            return this;
         }
 
-        public int getYear() {
-            return year;
+        public Builder setStatus(Status status) {
+            this.status = status;
+            return this;
         }
 
-        public Status getStatus() {
-            return status;
+        public Builder setCarType(CarType carType) {
+            this.carType = carType;
+            return this;
         }
 
+        public Car build() {
+            return new Car(this);
+        }
 
-        public static class Builder {
-            private String carId;
-            private String licensePlate;
-            private String make;
-            private String model;
-            private int year;
-            private Status status;
+        @Override
+        public String toString() {
+            return "Car{" +
+                    "carId='" + carId + '\'' +
+                    ", licensePlate='" + licensePlate + '\'' +
+                    ", make='" + make + '\'' +
+                    ", model='" + model + '\'' +
+                    ", year=" + year +
+                    ", status=" + status +
+                    ", carType=" + carType +
+                    '}';
+        }
 
-            public Builder setCarId(String carId) {
-                this.carId = carId;
-                return this;
-            }
-
-            public Builder setLicensePlate(String licensePlate) {
-                this.licensePlate = licensePlate;
-                return this;
-            }
-
-            public Builder setMake(String make) {
-                this.make = make;
-                return this;
-            }
-
-            public Builder setModel(String model) {
-                this.model = model;
-                return this;
-            }
-
-            public Builder setYear(int year) {
-                this.year = year;
-                return this;
-            }
-
-            public Builder setStatus(Status status) {
-                this.status = status;
-                return this;
-            }
-
-            public Car build() {
-                return new Car(this);
-            }
-
-            @Override
-            public String toString() {
-                return "Builder{" +
-                        "carId='" + carId + '\'' +
-                        ", licensePlate='" + licensePlate + '\'' +
-                        ", make='" + make + '\'' +
-                        ", model='" + model + '\'' +
-                        ", year=" + year +
-                        ", status=" + status +
-                        '}';
-            }
-
-            public static Builder fromCar(Car car) {
-                return new Builder()
-                        .setCarId(car.getCarId())
-                        .setLicensePlate(car.getLicensePlate())
-                        .setMake(car.getMake())
-                        .setModel(car.getModel())
-                        .setYear(car.getYear())
-                        .setStatus(car.getStatus());
-            }
-
+        public static Builder fromCar(Car car) {
+            return new Builder()
+                    .setCarId(car.getCarId())
+                    .setLicensePlate(car.getLicensePlate())
+                    .setMake(car.getMake())
+                    .setModel(car.getModel())
+                    .setYear(car.getYear())
+                    .setStatus(car.getStatus())
+                    .setCarType(car.getCarType());
         }
     }
-
-
-
-
-
-
-
-
-
+}
