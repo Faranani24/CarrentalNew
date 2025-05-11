3ec1b1d9869eba818af9a9d2f2e8aa5c15963c5d
InamJim <222086939@mycput.ac.za>
2025-05-11 13:08:17 +0200

    Domain for Review and Payment

--- src/main/java/co/za/carrental/domain/Review.java
+++ src/main/java/co/za/carrental/domain/Review.java
@@ -1,4 +1,80 @@
+/*  Review.java
+    Author:Inam Jim (222086939)
+    Date: 11 May 2025
+ */
 package co.za.carrental.domain;
 
-public class Review {
+
+
+public class Review extends Car{
+
+    //Sting reviewId  int rating(1-5)  String comment
+
+    private String reviewId;
+    private int rating;
+    private String comment;
+
+    private Review() {}
+
+    private Review(Builder builder) {
+        this.reviewId = builder.reviewId;
+        this.rating = builder.rating;
+        this.comment = builder.comment;
+    }
+
+    public String getReviewId() {
+        return reviewId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "reviewId='" + reviewId + '\'' +
+                ", rating=" + rating +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String reviewId;
+        private int rating;
+        private String comment;
+
+        public Builder setreviewId(String reviewId) {
+            this.reviewId = reviewId;
+            return this;
+        }
+        public Builder setrating(int rating) {
+            this.rating = rating;
+            return this;
+        }
+        public Builder setcomment(String comment) {
+            this.comment = comment;
+            return this;
+        }
+
+        public Review build() {
+            return new Review(this);
+        }
+
+        public Builder copy(Review review) {
+
+            if (review != null){
+                this.reviewId = review.getReviewId();
+                this.rating = review.getRating();
+                this.comment = review.getComment();
+            }
+            return this;
+        }
+
+    }
 }
