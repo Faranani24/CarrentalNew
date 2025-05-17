1fea8aeff5b7845aba62adda64fc91a9b9938cea
farananikhangale <230136982@mycput.ac.za>
2025-05-18 00:27:43 +0200

    Check commits

--- src/main/java/co/za/carrental/factory/CarFactory.java
+++ src/main/java/co/za/carrental/factory/CarFactory.java
@@ -1,4 +1,17 @@
 package co.za.carrental.factory;
 
+import co.za.carrental.domain.Car;
+
 public class CarFactory {
+    public static Car createCar(String carNumber, String carBrand, String carModel, String carColour, String carYear) {
+                return new Car.Builder()
+                .setCarId(carNumber)
+                .setLicensePlate(carNumber)
+                .setMake(carBrand)
+                .setModel(carModel)
+                .setYear(Integer.parseInt(carYear))
+                .setStatus(co.za.carrental.domain.Status.AVAILABLE)
+                        .build();
+    }
 }
+
