93f6a5011eece63ead61ccf813935c0943e7439d
Lance1103 <230803865@mycput.ac.za>
2025-05-17 21:01:57 +0200

    Factory commit

--- src/main/java/co/za/carrental/factory/BookingFactory.java
+++ src/main/java/co/za/carrental/factory/BookingFactory.java
@@ -1,4 +1,22 @@
 package co.za.carrental.factory;
 
+import co.za.carrental.*;
+import co.za.carrental.domain.Booking;
+
+
+import java.util.*;
+
 public class BookingFactory {
+    private static Enum status;
+
+    public static Booking createBooking(String bookingId, Date startDate, Date endDate, Float totalCost, Enum Status) {
+
+        return new Booking.Builder()
+                .setBookingId(bookingId)
+                .setStartDate(startDate)
+                .setEndDate(endDate)
+                .setTotalCost(totalCost)
+                .setStatus(status)
+                .build();
+    }
 }
