fa98d4b0b3d138c1e68fc77d0729d15a9a417ff6
InamJim <222086939@mycput.ac.za>
2025-05-18 01:57:12 +0200

    Factory Classes

--- src/main/java/co/za/carrental/factory/PaymentFactory.java
+++ src/main/java/co/za/carrental/factory/PaymentFactory.java
@@ -1,4 +1,15 @@
 package co.za.carrental.factory;
 
+import co.za.carrental.domain.Payment;
+
 public class PaymentFactory {
+
+    public static Payment createPayment(String paymentId, String paymentMethod, double amount) {
+        return new Payment.Builder()
+                .setpaymentID(paymentId)
+                .setPaymentMethod(Payment.paymentMethod.valueOf(paymentMethod))
+                .setAmount((float) amount)
+                .build();
+    }
+
 }
