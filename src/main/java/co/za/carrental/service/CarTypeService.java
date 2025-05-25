244e074af4ed376928bb09e3a1b4f1b3c4ddc960
farananikhangale <230136982@mycput.ac.za>
2025-05-25 15:44:13 +0200

    Tests run with no output at run time as of now

---
+++ src/main/java/co/za/carrental/service/CarTypeService.java
@@ -0,0 +1,14 @@
+package co.za.carrental.service;
+
+import co.za.carrental.domain.CarType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CarTypeService {
+    CarType create(CarType carType);
+    Optional<CarType> read(String typeId);
+    CarType update(CarType carType);
+    void delete(String typeId);
+    List<CarType> findAll();
+}
