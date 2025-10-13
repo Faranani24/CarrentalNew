92b6891043ac780cef9605301a3244d974f21d45
Lance1103 <230803865@mycput.ac.za>
2025-10-13 21:59:32 +0200

    Final project push to Carrentall repo

--- src/main/java/co/za/carrental/service/ICustomerService.java
+++ src/main/java/co/za/carrental/service/ICustomerService.java
@@ -1,7 +1,22 @@
 package co.za.carrental.service;
 
 import co.za.carrental.domain.Customer;
+import java.util.List;
+import java.util.Optional;
 
-public interface ICustomerService extends IService<Customer, String> {
-    // Add customer-specific service methods here if needed
+public interface ICustomerService {
+    Customer save(Customer customer);
+    Optional<Customer> findById(String customerId);
+    Customer update(Customer customer);
+    void deleteById(String customerId);
+    List<Customer> getAll();
+
+    Optional<Customer> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<Customer> findByEmailAndPassword(String email, String password);
+    boolean emailExist(String email);
+
+    boolean emailExists(String email);
 }
